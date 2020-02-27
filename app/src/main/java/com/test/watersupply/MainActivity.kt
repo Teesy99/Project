e4e6881fed2b425238f3bfb915535f21ef8ea10c
kumar-mukeshh <mknitmgh@gmail.com>
2020-02-27 21:13:18 +0530

    Initial commit

--- app/src/main/java/com/test/watersupply/MainActivity.kt
+++ app/src/main/java/com/test/watersupply/MainActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,12 +17,14 @@ class MainActivity : AppCompatActivity() {
 
 
 
+
         val dice_img : ImageView = findViewById(R.id.imageView)
         dice_img.setImageResource(R.drawable.water_distribution_pic1)
 
         val b1 : Button = findViewById(R.id.B1)
         b1.setOnClickListener {
             val intent= Intent(this, Show_data::class.java)
+            intent.putExtra("Username", "Name")
             startActivity(intent)
         }
 
--- app/src/main/java/com/test/watersupply/Show_data.kt
+++ app/src/main/java/com/test/watersupply/Show_data.kt
@@ -1,8 +1,13 @@
 package com.test.watersupply
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.SetOptions
+import java.time.LocalDateTime
 import java.util.*
 import kotlin.random.Random
 
@@ -13,29 +18,53 @@ class Show_data : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_show_data)
 
-        var database = FirebaseDatabase.getInstance()
-        val Refer = database.getReference("sensor")
+        var strUser: String = intent.getStringExtra("Username")
+
+        val database = FirebaseFirestore.getInstance()
+        //val Refer = database.getReference("sensor")
 
         val c = Calendar.getInstance()
         val dd = c.get(Calendar.DAY_OF_MONTH)
-        val mm = c.get(Calendar.MONTH)
+        val mm = c.get(Calendar.MONTH)+1
         val yy= c.get(Calendar.YEAR)
 
         val h = c.get(Calendar.HOUR_OF_DAY)
         val min = c.get(Calendar.MINUTE)
         val sec = c.get(Calendar.SECOND)
+        val  p :String="/"
+        val t : String=":"
+        var m : String= if(mm < 10) {
+             "0"+ mm.toString()
+        }
+        else {
+              mm.toString()
+        }
 
-        val da : String = yy.toString() + "-" + mm.toString()+ "-" + dd.toString()
-        val ti : String = h.toString() + "-" + min.toString()+ "-" + sec.toString()
+        val da : String =  yy.toString()+ p + m + p + dd.toString()
 
-        val press:Int = 9
-        val temp :Int= 100
+        val ti : String = h.toString() + t + min.toString()+ t + sec.toString()
 
-        val to: String? = Refer.push().key
+        val press: Int=9
+        val temp : Int= Random.nextInt()%1000
 
-        val data=sensor_data(ti,da,press,temp)
 
-        Refer.child(ti.toString()).setValue(data)
+        val city = hashMapOf(
+            "ti" to ti,
+            "da" to da,
+            "press" to press,
+            "temp" to temp
+        )
+
+        database.collection("sensor")
+            .add(city)
+
+
+        //val to: String? = Refer.push().key
+
+       // val data=sensor_data(ti,da,press,temp)
+
+        //Refer.child(to.toString()).setValue(data)
 
     }
 }
+
